8bf424204e8bd72d8a7a8266bd6a17eb27952040
Thiago S Fukushima <thiago.shf@outlook.com>
2025-07-13 20:24:31 -0300

    Adicionando css

--- src/Login.jsx
+++ src/Login.jsx
@@ -4,15 +4,19 @@ import './css/Login.css';
 const Login = () => {
     return (
         <>
-            <h1 className="title-Login">Login</h1>
+        <div className="container">
+            <h1 className="LogoType">Wellcome to FinBalance</h1>
             <form>
-                <div className="userLogin-Form-Control">
-                    <input id="email" type="email" name="Email" placeholder="E-mail" requerid="true"/>
-                    <input id="password" type="password" name="password" placeholder="Password" requerid="true"/>
-                </div>
+                <h2 className="title-Login">Login</h2>
+                    <div className="userLogin-Form-Control">
+                        <input id="email" type="email" name="Email" placeholder="E-mail" requerid="true"/>
+                        <input id="password" type="password" name="password" placeholder="Password" requerid="true"/>
+                    </div>
                 <button className="userLogin-submit-button" type="submit">Sign In</button>
+                <p>Don't have an account yet? <a href="/Register">Create Account</a></p>
             </form>
-            <p>Don't have an account yet? <a href="/Register">Create Account</a></p>
+            
+        </div>
         </>
     )
 };
--- src/Register.jsx
+++ src/Register.jsx
@@ -1,18 +1,22 @@
 import { useState } from 'react';
+import './css/Register.css';
 
 const Register = () => {
     return(
         <>
-            <h1 className="title-Register">Register User</h1>
-            <form>
-                <div className="userRegister-form-control">
-                    <input id="email" type="email" name="email" placeholder="E-mail" required="true"/>
-                    <input id="password" type="password" name="password" placeholder="Password" required="true"/>
-                </div>
-                <button className="userRegister-create-button" type="submit">Sign Up</button>
-            </form>
-
-            <p>Already have an account? <a href="/Login">Log In</a></p>
+            <div className="container">
+                <h1>Join FinBalance</h1>
+                    <form>
+                        <h2 className="title-Register">Register User</h2>
+                        <div className="userRegister-form-control">
+                            <input id="email" type="email" name="email" placeholder="E-mail" required="true"/>
+                            <input id="password" type="password" name="password" placeholder="Password" required="true"/>
+                            <input id="confirmPassword" type="password" name="password" placeholder="Confirm Password" required="true"/>
+                        </div>
+                        <button className="userRegister-create-button" type="submit">Sign Up</button>
+                        <p>Already have an account? <a href="/Login">Log In</a></p>
+                    </form>
+            </div>
         </>
     )
 }
