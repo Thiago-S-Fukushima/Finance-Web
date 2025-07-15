27ba3a3e24db1cdfdc0dc806a390b1e8bc50de89
Thiago S Fukushima <thiago.shf@outlook.com>
2025-07-14 23:48:05 -0300

    Adicionando rotas para o back

--- src/Login.jsx
+++ src/Login.jsx
@@ -2,6 +2,17 @@ import { useState } from 'react';
 import './css/Login.css';
 
 const Login = () => {
+    const [loginData, setLoginData] = useState ({
+    username:"",
+    password:""
+});
+
+const toLogin = () => {
+    e.preventDefault();
+
+    axios.post('',loginData)
+}
+
     return (
         <>
         <div className="container">
@@ -9,10 +20,13 @@ const Login = () => {
             <form>
                 <h2 className="title-Login">Login</h2>
                     <div className="userLogin-Form-Control">
-                        <input id="email" type="email" name="Email" placeholder="E-mail" requerid="true"/>
-                        <input id="password" type="password" name="password" placeholder="Password" requerid="true"/>
+                        <input id="email" value={loginData.username} onChange={(e) => setLoginData({...loginData, username: e.target.value})} 
+                        type="email" name="Email" placeholder="E-mail" required="true"/>
+
+                        <input id="password" value={loginData.password} onChange={(e) => setLoginData({...loginData, password: e.target.value})} 
+                        type="password" name="password" placeholder="Password" required="true"/>
                     </div>
-                <button className="userLogin-submit-button" type="submit">Sign In</button>
+                <button className="userLogin-submit-button" onClick={() => toLogin()}>Sign In</button>
                 <p>Don't have an account yet? <a href="/Register">Create Account</a></p>
             </form>
             
